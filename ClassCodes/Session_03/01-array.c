d26c9010a7c3de2261ac86c8ce4f51dcee144630
Sarthak <sarthakajaiswal@gmail.com>
2025-10-08 10:45:17 +0530

    session 3

---
+++ ClassCodes/Session_03/01-array.c
@@ -0,0 +1,53 @@
+/*
+    @GOAL:  starting from last element, shift the elements of array 
+            to the right until negative element is occured 
+    @DATE:  05 OCT 2025 
+    @AUTHOR: Sarthak A. Jaiswal 
+*/
+
+#include <stdio.h> 
+
+void display(int arr[], int size); 
+
+int main(void) 
+{
+    // variable declarations 
+    int a[8] = {-15, -5, -8, 20, 10, 50, 40, 80}; 
+
+    printf("ARRAY BEFORE SHIFTING POSITIVE NUMBERS TO THE RIGHT:\n"); 
+    display(a, 8); 
+
+    int tmp = a[7]; // save the last indexed element in tmp variable 
+    int i = 6;      // initialize i to the second last element 
+
+    // keep shifting array elements to their right index 
+    // until negative element is encountered 
+    // Assumptions: There is atleast one negative number 
+    while(a[i] >= 0) 
+    {
+        a[i+1] = a[i]; 
+        i = i - 1; 
+    } 
+    a[i+1] = tmp;   // save the last indexed element 
+                    // at the first positive index 
+    printf("ARRAY AFTER SHIFTING POSITITVE NUMBERS TO THE RIGHT:\n"); 
+    display(a, 8); 
+
+    return (0); 
+} 
+
+void display(int arr[], int size) 
+{
+    // variable declarations 
+    int i; 
+
+    // code 
+    i = 0; 
+    while(i < size) 
+    {
+        printf("arr[%d]: %d\n", i, arr[i]); 
+        i = i + 1; 
+    } 
+}
+
+
---
+++ ClassCodes/Session_03/02-array.c
@@ -0,0 +1,70 @@
+/*
+    @GOAL:  You will be given a non-empty array of integers. 
+            The array is filled in such a way that all negative 
+            numbers are stacked on the left side of the array and 
+            all non-negative numbers are stacked on the right 
+            side of the array. 
+            Your job is to right shift and rotate all non-negative 
+            elements. 
+    @boundaty-case: There may not be any negative element, in that 
+            case the entire array will be right shifted and rotated. 
+
+    @DATE   05 OCT 2025
+    @AUTHOR Sarthak A. Jaiswal 
+*/
+
+#include <stdio.h> 
+
+void display(int arr[], int size); 
+void right_shift_and_rotate(int arr[], int size); 
+
+int main(void) 
+{
+    int a[8] = {-10, -5, -8, -3, 30, 40, 20, 10}; 
+    int b[8] = {30, 10, 2, 25, 30, 16, 28, 40}; 
+
+    printf("Showing array \'a\' BEFORE right shift and rotate: \n"); 
+    display(a, 8); 
+    right_shift_and_rotate(a, 8); 
+    printf("Showing array \'a\' AFTER right shift and rotate:\n"); 
+    display(a, 8); 
+
+    printf("Showing array \'b\' BEFORE right shift and rotate:\n"); 
+    display(b, 8); 
+    right_shift_and_rotate(b, 8); 
+    printf("Showing array \'b\' AFTER right shift and rotate:\n"); 
+    display(b, 8); 
+
+    return (0); 
+} 
+
+void right_shift_and_rotate(int arr[], int size) 
+{
+    // variable declarations 
+    int i; 
+    int tmp = arr[size-1]; 
+
+    // code
+    i = size-2; 
+    while(i >= 0 && arr[i] > 0) 
+    {
+        arr[i+1] = arr[i]; 
+        i = i - 1; 
+    } 
+    arr[i+1] = tmp; 
+} 
+
+void display(int arr[], int size) 
+{
+    // variable declarations 
+    int i; 
+
+    // code 
+    i = 0; 
+    while(i < size) 
+    {
+        printf("arr[%d]: %d\n", i, arr[i]); 
+        i = i + 1; 
+    } 
+} 
+
