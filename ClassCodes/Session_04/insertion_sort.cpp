f54632f9722c2dc6891df4e2c7a3ef0a95736610
Sarthak <sarthakajaiswal@gmail.com>
2025-11-12 09:58:07 +0530

    session-04:completes pending code-insertion_sort.cpp

---
+++ ClassCodes/Session_04/insertion_sort.cpp
@@ -0,0 +1,81 @@
+/* 
+    @GOAL:  insertion_sort() implementation using C++ 
+    @DATE:  12 NOV 2025 
+    @AUTHOR: Sarthak A. Jaiswal 
+*/
+
+#include <iostream> 
+#include <cassert> 
+#include <cstdlib> 
+
+namespace cpa 
+{
+    class array 
+    {
+        private: 
+            int* a; 
+            int N; 
+
+            void insert_at_sorting_position(int size) 
+            {
+                int tmp = a[size-1]; 
+                int i = size-2; 
+
+                while(i > -1 && a[i] > tmp)
+                {
+                    a[i+1] = a[i]; 
+                    i = i - 1; 
+                } 
+                a[i+1] = tmp; 
+            } 
+
+        public: 
+            array(int N) 
+            {
+                assert(N >= 0); 
+                a = new int[N]; 
+                this->N = N; 
+            } 
+
+            ~array() 
+            {
+                delete[] a; 
+            } 
+
+            int& operator[](int index) 
+            {
+                assert(index >= 0 && index < N); 
+                return *(a+index); 
+            } 
+
+            friend std::ostream& operator<<(std::ostream& os, const array& arr); 
+
+            void sort() 
+            {
+                for(int subarray_size = 2; subarray_size <= N; ++subarray_size) 
+                    insert_at_sorting_position(subarray_size); 
+            } 
+    }; 
+
+    std::ostream& operator<<(std::ostream& os, const cpa::array& arr) 
+    {
+        for(int i = 0; i < arr.N; ++i) 
+            std::cout << "a[" << i << "]: " << arr.a[i] << std::endl; 
+        return os; 
+    } 
+}; 
+
+int main(void) 
+{
+    cpa::array A(8); 
+
+    for(int i = 0; i < 8; ++i) 
+        A[i] = rand(); 
+
+    std::cout << "BEFORE SORT: " << std::endl << A; 
+    A.sort(); 
+    std::cout << "AFTER SORT: " << std::endl << A; 
+
+    return 0; 
+} 
+
