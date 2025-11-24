79fece2a922144df274744b2d76e637747819f5f
Sarthak Jaiswal <sarthakajaiswal@gmail.com>
2025-11-24 10:39:56 +0530

    live coding: Session 19

---
+++ ClassCodes/Session_19/01-traversing-through-list.c
@@ -0,0 +1,132 @@
+/*
+    @GOAL: To study list transversal patterns 
+    @DATE: 23 Nov 2025 
+    @AUTHOR: Sarthak A. Jaiswal 
+*/
+
+#include <stdio.h> 
+
+struct node 
+{
+    int data; 
+    struct node* next; 
+}; 
+
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    }
+} 
+
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run!= NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    { 
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    }
+} 
+
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+void get_last_node(struct node* p_list) 
+{
+    struct node* run = NULL; 
+    run = p_list; 
+    while(run != NULL) 
+    {
+        run = run->next; 
+    } 
+
+    // run is the last node 
+} 
+
+void get_last_node (struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list; 
+    while(run->next != NULL) 
+    {
+        run = run->next; 
+    } 
+} 
+
+void get_last_node (struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list; 
+    while(run->next != NULL) 
+    {
+        run = run->next; 
+    }
+    
+} 
+
+void get_last_node(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list; 
+    while(run->next != NULL) 
+    {
+        run = run->next; 
+    }
+} 
+
+void get_last_node(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list; 
+    while(run->next != NULL)
+    {
+        run = run->next; 
+    } 
+
+    // run is the last node 
+}
