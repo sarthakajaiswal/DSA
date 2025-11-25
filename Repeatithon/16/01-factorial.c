3d30fc436082d32d51fbded3fd0afa5e090659bc
Sarthak <sarthakajaiswal@gmail.com>
2025-11-25 11:05:06 +0530

    Repeatithon: session 16

---
+++ Repeatithon/16/01-factorial.c
@@ -0,0 +1,107 @@
+#include <stdio.h> 
+
+int factorial(int n); 
+
+int main(void) 
+{ 
+    printf("fatorial(5) = %d\n", factorial(5)); 
+    return(0); 
+}
+
+int factorial(int N) 
+{
+    if(N == 0) 
+        return (1); 
+
+    int tmp = factorial(N-1); 
+    return (N * tmp); 
+} 
+
+//========================= 
+#include <stdio.h> 
+#include <stdio.h> 
+#include <stdio.h> 
+#include <stdio.h> 
+#include <stdio.h> 
+
+int factorial(int N); 
+int factorial(int N); 
+int factorial(int N); 
+int factorial(int N); 
+int factorial(int N); 
+
+int main(void) 
+{
+    printf("factorial(5) = %d\n", factorial(5)); 
+    return (0); 
+} 
+
+int main(void) 
+{
+    printf("factorial(5) = %d\n", factorial(5)); 
+    return (0); 
+}
+
+int main(void) 
+{
+    printf("factorial(5) = %d\n", factorial(5)); 
+    return (0); 
+} 
+
+int main(void) 
+{
+    printf("factorial(5) = %d\n", factorial(5)); 
+    return (0); 
+} 
+
+int main(void) 
+{ 
+    printf("factorial(5) = %d\n", factorial(5)); 
+    return (0); 
+}
+
+int factorial(int N) 
+{
+    // terminating condition 
+    if(N == 0) 
+        return (1); 
+
+    int tmp = factorial(N-1); 
+    return (N * tmp); 
+} 
+
+int factorial(int N) 
+{
+    // terminating condtion 
+    if(N == 0) 
+        return (1); 
+
+    int tmp = factorial(N-1); 
+    return (N * tmp); 
+} 
+
+int factorial(int N) 
+{ 
+    // terminating condition 
+    if(N == 0) 
+        return (1); 
+
+    int tmp = factorial(N-1); 
+    return (N * tmp); 
+}
+
+int factorial(int N) 
+{
+    // terminating condition 
+    if(N == 0) 
+        return (1); 
+
+    int tmp = factorial(N-1); 
+    return (N * tmp); 
+} 
+
+int factorial(int N) 
+{
+    printf("factorial(5) = %d\n", factorial(5)); 
+    return (0); 
+} 
