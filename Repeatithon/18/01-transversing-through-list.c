6608cbe5233373999ac9440552a14d5e4361dc71
Sarthak Jaiswal <sarthakajaiswal@gmail.com>
2025-12-03 12:47:38 +0530

    repeatihon:session18 in progress

---
+++ Repeatithon/18/01-transversing-through-list.c
@@ -0,0 +1,330 @@
+#include <stdio.h> 
+
+struct node
+{
+    int data; 
+    struct node* next; 
+}; 
+
+// 1
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data);  
+        run = run->next; 
+    } 
+} 
+
+// 2
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    }
+}
+
+// 3
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 4
+void show_list(struct node* p_list) 
+{
+    struct node* run =  NULL; 
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 5
+void show_list(struct node* p_list) 
+{
+    struct node* run= NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 6
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->data; 
+    } 
+} 
+
+// 7
+void show_list(struct node* p_list) 
+{
+    struct node* run; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->data; 
+    } 
+} 
+
+// 8
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 9
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 10
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 11 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 12 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 13 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 14 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    }
+} 
+
+// 15 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 16 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 17 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 18 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 19 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 20 
+void show_list(struct node* p_list) 
+{ 
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+}
+
+// 21 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 22 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 23 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 24 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
+
+// 25 
+void show_list(struct node* p_list) 
+{
+    struct node* run = NULL; 
+
+    run = p_list->next; 
+    while(run != NULL) 
+    {
+        printf("run->data = %d\n", run->data); 
+        run = run->next; 
+    } 
+} 
